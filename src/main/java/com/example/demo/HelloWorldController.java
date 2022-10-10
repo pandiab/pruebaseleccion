f19a721113b33aa6fa953bee270d9eb70cf02d46
pandiab <paulina.andia@gmail.com>
2022-10-09 23:42:25 -0300

    Cambio mensaje

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")  
     public String hello() {  
-        return "Hello InstanceLatam";  
+        return "Hello InstanceLatam :)";  
     }
 
     @RequestMapping("/ping")  
