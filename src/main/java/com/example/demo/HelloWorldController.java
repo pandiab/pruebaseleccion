a32be120f404ed09496d6a9ef6ca64c4ffe555e9
pandiab <paulina.andia@gmail.com>
2022-10-10 00:17:34 -0300

    Cambio en el mensaje de despliegue

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")  
     public String hello() {  
-        return "Hello InstanceLatam :)";  
+        return "Holanda, que Talca InstanceLatam :)";  
     }
 
     @RequestMapping("/ping")  
